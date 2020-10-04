75dc0627db6ce42085e15c24882c25b69c5f1254
Aaron Penn <aaron@pennpro.it>
2020-10-04 21:55:45 +0200

    copied all CSI files

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -189,7 +189,8 @@ define KernelPackage/ath9k-common
   DEPENDS+= @PCI_SUPPORT||USB_SUPPORT||TARGET_ath79 +kmod-ath +@DRIVER_11N_SUPPORT +@DRIVER_11W_SUPPORT
   FILES:= \
 	$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath9k/ath9k_common.ko \
-	$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath9k/ath9k_hw.ko
+	$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath9k/ath9k_hw.ko \
+	$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath9k/ar9003_csi.ko
 endef
 
 define KernelPackage/ath9k
